32248521f7e243632f8fbcf34aed5b8b0ebaf236
Joaquin Pedeuboy <joaquin.pedeuboy@hotmail.com>
2026-05-08 17:10:00 -0300

    Server output

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,6 +13,6 @@ export default defineConfig({
     image: {
         domains: ['lamarywordpress.test', 'lightgoldenrodyellow-camel-304621.hostingersite.com']
     },
-
+    output: 'server',
     adapter: netlify()
 });
